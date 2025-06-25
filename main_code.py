3d1583b92f82e1d265975af3d358b43c13a3cde7
Friedrice-1 <allenjayansing99@gmail.com>
2025-06-25 19:50:24 +0800

    Pops up a window when program is ran

--- main_code.py
+++ main_code.py
@@ -1,2 +1,24 @@
+# Import libraries
 import pygame
 import random
+
+# Initialize pygame
+pygame.init()
+
+# Screen setup
+
+WITDH, HEIGHT = 600 , 400
+screen = pygame.display.set_mode((WITDH, HEIGHT))
+pygame.display.set_caption("Dodge the red falling block.")
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# Game loop (shows the window)
+running = True
+while running:
+    screen.fill(WHITE)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
