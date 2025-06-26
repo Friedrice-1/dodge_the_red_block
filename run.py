967befa5cc82a9d98001887daea83bc0404a6af2
Friedrice-1 <allenjayansing99@gmail.com>
2025-06-26 17:01:25 +0800

    Imported necessary libraries and initialized pygame

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 # run.py
+
 # Import from the Player and Enemy class file and import pygame 
+from player_and_enemy import Player, Enemy
+import pygame
+
 # Initialize pygame
+pygame.init()
 # Screen and color setup
 # Clock and font
 # Collision detection
