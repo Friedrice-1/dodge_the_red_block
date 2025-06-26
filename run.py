6c15dfe6a96e45d9083c3b992258eb72224aeaa9
Friedrice-1 <allenjayansing99@gmail.com>
2025-06-26 16:58:27 +0800

    Pseudocode for the run file

---
+++ run.py
@@ -0,0 +1,8 @@
+# run.py
+# Import from the Player and Enemy class file and import pygame 
+# Initialize pygame
+# Screen and color setup
+# Clock and font
+# Collision detection
+# Create entities
+# Game loop
