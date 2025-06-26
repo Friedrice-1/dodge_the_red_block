abf1627a5ca3f9841517b8e49de9c0039741bbc8
Friedrice-1 <allenjayansing99@gmail.com>
2025-06-26 16:46:33 +0800

    Added player class and initialized

--- player_and_enemy.py
+++ player_and_enemy.py
@@ -9,4 +9,9 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 # Create a player class
+class Player:
+    def __init__(self, size = 50, speed = 5):
+        self.size = size
+        self.speed = speed
+        self.pos = [WIDTH // 2, HEIGHT - size]
 # Create the enemy class
