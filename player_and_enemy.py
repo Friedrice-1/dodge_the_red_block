c4408bf90eb7aeac621fc5992c11f4b33a72d655
Friedrice-1 <allenjayansing99@gmail.com>
2025-06-26 16:44:36 +0800

    Imported the libraries and the constants from the main code

--- player_and_enemy.py
+++ player_and_enemy.py
@@ -1,6 +1,12 @@
 # Entities such as player and the enemy
 
 # Import the libraries
+import pygame
+import random
 # Import the constants
+WIDTH, HEIGHT = 600 , 400
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
 # Create a player class
 # Create the enemy class
